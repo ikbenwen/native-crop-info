1259ea1f4adbeda559bb6174e3b8215b49cda3ad
ikbenwen <w.michels@novi-education.nl>
2021-02-04 16:41:31 +0100

    api request installed

--- App.js
+++ App.js
@@ -3,10 +3,19 @@ import { Button, View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
+const fetch = require('node-fetch');
+
+
+(async () => {
+    const response = await fetch('https://trefle.io/api/v1/plants/search?token=BC09Zl9KrfbB6rTcw_kq-YBdKRpfjYITLdotI3wrZNc&q=Strawberry-tomato');
+    const json = await response.json();
+    console.log(json);
+})();
+
 function HomeScreen({ navigation }) {
   return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen!</Text>
+        <Text>Home Screen</Text>
         <Button
             title="Go to Details"
             onPress={() => navigation.navigate('Details')}
