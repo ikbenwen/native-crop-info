baeabf9b30363e179b2f26425232615f57aad2bc
ikbenwen <w.michels@novi-education.nl>
2021-02-05 13:52:20 +0100

    display plants in flatlist

--- App.js
+++ App.js
@@ -14,25 +14,25 @@ const fetch = require('node-fetch');
     console.log(json);
 })();
 
-function HomeScreen({ navigation }) {
-  return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Home Screen</Text>
-        <Button
-            title="Go to Details"
-            onPress={() => navigation.navigate('Details')}
-        />
-      </View>
-  );
-}
+// function HomeScreen({ navigation }) {
+//   return (
+//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//         <Text>Home Screen</Text>
+//         <Button
+//             title="Go to Details"
+//             onPress={() => navigation.navigate('Details')}
+//         />
+//       </View>
+//   );
+// }
 
-function DetailsScreen() {
-  return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details Screen</Text>
-      </View>
-  );
-}
+// function DetailsScreen() {
+//   return (
+//       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+//         <Text>Details Screen</Text>
+//       </View>
+//   );
+// }
 
 const Stack = createStackNavigator();
 
@@ -40,7 +40,7 @@ function App() {
   return (
       <NavigationContainer>
         <Stack.Navigator initialRouteName="Home">
-          <Stack.Screen name="Home" component={HomeScreen} />
+          {/*<Stack.Screen name="Home" component={HomeScreen} />*/}
           <Stack.Screen name="Plants" component={PlantIndex} />
         </Stack.Navigator>
       </NavigationContainer>
--- screens/PlantIndex.js
+++ screens/PlantIndex.js
@@ -4,34 +4,46 @@ import axios from 'axios';
 
 
 // const fetch = require('node-fetch');
-
-
+//
 // (async () => {
-//     const response = await fetch('https://trefle.io/api/v1/plants?token=BC09Zl9KrfbB6rTcw_kq-YBdKRpfjYITLdotI3wrZNc');
+//     const response = await fetch('https://trefle.io/api/v1/plants?filter_not%5Bedible_part%5D=null&token=BC09Zl9KrfbB6rTcw_kq-YBdKRpfjYITLdotI3wrZNc&limit=5');
 //     const json = await response.json();
 //     console.log(json);
 // })();
+//
+
 
 export default function PlantIndex () {
+
+
     const [plants, setPlants] = useState([])
 
     useEffect(() => {
         async function getPlants(){
             try {
-                const result = await axios.get('https://trefle.io/api/v1/plants?filter_not%5Bedible_part%5D=null&token=BC09Zl9KrfbB6rTcw_kq-YBdKRpfjYITLdotI3wrZNc&limit=5')
+                const {data: {data}} = await axios.get('https://trefle.io/api/v1/plants?filter_not%5Bedible_part%5D=null&token=BC09Zl9KrfbB6rTcw_kq-YBdKRpfjYITLdotI3wrZNc');
 
-               setPlants(result);
-                console.log(result)
+               setPlants(data);
+                console.log(data)
             } catch (e) {
                 console.log(e)
             }
         }
         getPlants()
-    }, [])
+    }, [''])
 
     return(
-        <view>
-            <text>Plant Index</text>
-        </view>
-    )
+        <View>
+            <Text>Plant Index</Text>
+
+
+            <FlatList data={plants}
+                      renderItem={({item}) => {
+                          return <Text>{item.common_name}</Text>;
+                      }}
+                      keyExtractor={(item) => item.id}>
+
+            </FlatList>
+        </View>
+    );
 }
